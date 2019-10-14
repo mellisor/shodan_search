7b9745989dbf5a18e5bdcd552551504b03c84fff
mellisor <evanmellisor@gmail.com>
2019-10-14 10:13:44 -0400

    Added feature to return all attributes

--- shodan_client.py
+++ shodan_client.py
@@ -5,6 +5,7 @@ import pprint
 import collections
 from time import sleep
 from configparser import ConfigParser
+import json
 
 class Shodan_Client(shodan.Shodan):
 
@@ -33,28 +34,31 @@ class Shodan_Client(shodan.Shodan):
         for ip in ips:
             sleep(wait)
             print(ip)
-            try:
-                result = self.host([ip])
-                matched = True
-                # This needs to be redone
-                if filter is not None:
-                    matched = False
-                    for key in filter.keys():
-                        for value in filter[key]:
-                            result[key] = list(map(lambda x: str(x), result[key]))
-                            if value in result[key]:
-                                matched = True
-                                break
-                        if matched:
+            result = self.host([ip])
+            matched = True
+            if filter is not None:
+                matched = False
+                for key in filter.keys():
+                    for value in filter[key]:
+                        # converts values in list to strings
+                        result[key] = list(map(lambda x: str(x), result[key]))
+                        if value in result[key]:
+                            matched = True
                             break
-                # End needs to be redone
-                if matched:
-                    item = {}
+                    if matched:
+                        break
+            if matched:
+                item = {}
+                if "all" in attrs:
+                    item = result
+                else:
                     for attr in attrs:
-                        item[attr] = result[attr]
-                    matches[ip] = item
-            except Exception as e:
-                print(e)
+                        try:
+                            # TODO: add alias for attributes
+                            item[attr] = result[attr]
+                        except Exception as e:
+                            print("{} not found".format(e))
+                matches[ip] = item
         return matches
 
     def compress_ports(self,query,pages=1,wait=1,filter=None):
@@ -99,12 +103,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    # TODO: Add alias argument
     parser.add_argument('range',help="IP range to scan")
     parser.add_argument('-a','--attributes',default='ports',help="Attributes to return")
     parser.add_argument('--ports',action='store_true',help="Only return ports, much faster but can only filter by ports")
     parser.add_argument('-f','--filter',default=None,help="Filter by attributes. Ex: -f ports:22,3389")
     parser.add_argument('-c','--csv',help="Output CSV file. EX: -c out.csv")
     parser.add_argument('-p','--pages',default=1,type=int,help="Number of pages to get results from")
+    parser.add_argument('-o','--output',help="Output results as json. EX: -o out.json")
     args = parser.parse_args()
 
     conf = ConfigParser()
@@ -126,8 +132,10 @@ if __name__ == "__main__":
         matches = s.compress_ports("net:%s" % args.range,pages=args.pages,filter=fltr)
     else:
         attrs = args.attributes.split(',')
-
         matches = s.filter_hosts(args.range,filter=fltr,attrs=attrs,pages=args.pages)
     pprint.pprint(matches)
     if args.csv:
         s.write_csv(matches,args.csv)
+    if args.output:
+        with open(args.output,'w+') as f:
+            json.dump(matches,f)
